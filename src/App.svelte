22644576a7ed2afb4b1f7a15827c363a84780b96
Arthur Vos <arthurvos06@gmail.com>
2023-08-06 20:50:02 +0200

    add animation scroll effects

--- src/App.svelte
+++ src/App.svelte
@@ -23,57 +23,99 @@
     });
   });
 
+  document.addEventListener('DOMContentLoaded', () => {
   const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      const id = entry.target.getAttribute("id");
-      if (entry.intersectionRatio > 0) {
-        entry.target.classList.add("active");
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.setAttribute("style", "opacity: 1; filter: blur(0); transform: translateX(0);")
       } else {
-        entry.target.classList.remove("active");
+        if(entry.target == document.querySelector(".anim")){
+          entry.target.setAttribute("style", "opacity: 0; filter: blur(5px); transform: translateX(-100%);")
+        } else if(entry.target == document.querySelector(".anim2")){
+          entry.target.setAttribute("style", "opacity: 0; filter: blur(5px); transform: translateX(100%);")
+        }
       }
     });
   });
 
   const animElements = document.querySelectorAll(".anim");
-  animElements.forEach(el => {
-    observer.observe(el);
-  });
+  const animElements2 = document.querySelectorAll(".anim2");
+  animElements.forEach((el) => observer.observe(el));
+  animElements2.forEach((el) => observer.observe(el));
+});
 
 </script>
 <main>
     <div id="fullpage">
-      <div class="section bg-neutral-950">
+      <div class="section ">
         <div class="grid sm:grid-cols-2 ">
           <div class="flex items-center justify-center h-screen">
-            <h1 class=" title text-white text-center text-8xl">Arthur Vos</h1>
+            <h1 class="anim title text-slate-200 text-center text-8xl">Arthur Vos</h1>
           </div>
           <div>
-            
+            <img class="ml-12 mt-10 anim2" width="80%" src="../src/assets/images/coding.svg" alt="svg">
           </div>
         </div>
       </div>
-      <div class="section">
-        <h1 class=" text-xl">dwddwdw</h1>   
+      <div class="section ">
+        <div class="grid sm:grid-cols-2 ">
+          <div class="flex items-center justify-center h-screen">
+            <div class="anim title text-slate-200 text-center text-8xl">
+              <h1
+        class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+      >
+        Understand User Flow.
+
+        <span class="sm:block"> Increase Conversion. </span>
+      </h1>
+            </div>
+          </div>
+          <div>
+            <div class="ml-12 mt-10 anim2">
+              
+            </div>
+          </div>
+        </div>
       </div>
       <div class="section">Some section</div>
       <div class="section">Some section</div>
   </div>
 </main>
 <style>
+  :root{
+    background-color: rgb(23, 23, 23);
+  }
   h1{
     font-family: league;
   }
 
   .title {
-  box-shadow: inset 0 0 0 0 black;
-  color: white;
+  box-shadow: inset 0 0 0 0 rgb(23, 23, 23);
+  
   margin: 0 -.1rem;
   padding: 0 .1rem;
   padding-top: 1rem;  
   transition: color .6s ease-in-out, box-shadow .5s ease-in-out;
 }
 .title:hover {
-  box-shadow: inset 1000px 0 0 0 white;
-  color: black;
+  box-shadow: inset 1000px 0 0 0 rgb(226, 232, 240);
+  color: rgb(23, 23, 23);
 }
+
+.anim {
+    opacity: 0;
+    filter: blur(5px);
+    transform: translateX(-100%);
+    transition: all 1s;
+}
+.anim2 {
+    opacity: 0;
+    filter: blur(5px);
+    transform: translateX(100%);
+    transition: all 1s;
+}
+
+
+
+
 </style>
