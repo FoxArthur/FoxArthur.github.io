82ce90c87bd49ba8c356800cfa2cd497ffde00ab
Arthur Vos <arthurvos06@gmail.com>
2023-08-10 17:05:01 +0200

    add timeline

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,8 @@
   import { onMount } from "svelte";
   import "fullpage.js/dist/fullpage.css";
   import fullpage from "fullpage.js";
+  import EventListener from "./components/eventListener.svelte";
+  import Timeline from "./components/timeline.svelte";
   
   
 
@@ -23,59 +25,79 @@
     });
   });
 
-  document.addEventListener('DOMContentLoaded', () => {
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.setAttribute("style", "opacity: 1; filter: blur(0); transform: translateX(0);")
-      } else {
-        if(entry.target == document.querySelector(".anim")){
-          entry.target.setAttribute("style", "opacity: 0; filter: blur(5px); transform: translateX(-100%);")
-        } else if(entry.target == document.querySelector(".anim2")){
-          entry.target.setAttribute("style", "opacity: 0; filter: blur(5px); transform: translateX(100%);")
-        }
-      }
-    });
-  });
-
-  const animElements = document.querySelectorAll(".anim");
-  const animElements2 = document.querySelectorAll(".anim2");
-  animElements.forEach((el) => observer.observe(el));
-  animElements2.forEach((el) => observer.observe(el));
-});
+  
 
 </script>
+
+<EventListener />
+
 <main>
     <div id="fullpage">
       <div class="section ">
         <div class="grid sm:grid-cols-2 ">
           <div class="flex items-center justify-center h-screen">
-            <h1 class="anim title text-slate-200 text-center text-8xl">Arthur Vos</h1>
+            <h1 class="anim-right title text-slate-200 text-center text-8xl">Arthur Vos</h1>
           </div>
           <div>
-            <img class="ml-12 mt-10 anim2" width="80%" src="../src/assets/images/coding.svg" alt="svg">
+            <img class="ml-12 mt-10 anim-left" width="80%" src="../src/assets/images/coding.svg" alt="svg">
           </div>
         </div>
       </div>
       <div class="section ">
+        <div class="flex items-center justify-center h-screen">
+          <section>
+            <div class=" px-4  sm:px-6 lg:px-8">
+              <div class="grid grid-cols-1 h-screen lg:grid-cols-2">
+                <div class="relative z-10 lg:py-16">
+                  <div class="relative h-64 sm:h-80 lg:h-full">
+                    <img
+                      alt="House"
+                      src="../src/assets/images/gala-crop.jpg"
+                      class="anim-right2 absolute inset-0 h-full w-full object-cover"
+                    />
+                  </div>
+                </div>
+          
+                <div class=" relative flex items-center ">
+                  <div class="anim-left2 lg:py-48 bg-neutral-800">
+                    <span
+                    class="hidden lg:absolute lg:inset-y-0 lg:-start-16 lg:block lg:w-16 lg:bg-neutral-800"
+                  ></span>
+          
+                  <div class="p-8 sm:p-16 lg:p-16">
+                    <h2 class="text-2xl font-bold sm:text-3xl text-slate-200">
+                      Who am I?
+                    </h2>
+          
+                    <p class="mt-4 text-slate-300">
+                      My name is Arthur Vos i'm a 17 year old Software Development student from The Netherlands.
+                      I've been programming for about 3 years now, I started when i was 14 years old and i've been doing it ever since.
+                      I made this site to show off some of my skills and to tell something about myself.
+                    </p>
+          
+                    
+                  </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          
+        </div>
+      </div>
+      <div class="section">
+
         <div class="grid sm:grid-cols-2 ">
           <div class="flex items-center justify-center h-screen">
-            <div class="anim title text-slate-200 text-center text-8xl">
-              <h1
-        class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
-      >
-        Understand User Flow.
-
-        <span class="sm:block"> Increase Conversion. </span>
-      </h1>
+            <div class="anim-right ">
+              <Timeline/>
             </div>
           </div>
           <div>
-            <div class="ml-12 mt-10 anim2">
-              
-            </div>
+            <img class="ml-12 mt-40 anim-left rounded-lg shadow-none transition-shadow duration-100 ease hover:shadow-lg hover:shadow-slate-700 cursor-pointer "  width="90%" src="https://www.hoornbeeck.nl/app/uploads/2020/04/2021-10-14-Hoornbeeck-Amersfoort-originele-kwaliteit-165.jpg" alt="svg">
           </div>
         </div>
+        
       </div>
       <div class="section">Some section</div>
       <div class="section">Some section</div>
@@ -85,7 +107,7 @@
   :root{
     background-color: rgb(23, 23, 23);
   }
-  h1{
+  h1, h2{
     font-family: league;
   }
 
@@ -102,18 +124,19 @@
   color: rgb(23, 23, 23);
 }
 
-.anim {
+.anim-right, .anim-right2 {
     opacity: 0;
     filter: blur(5px);
     transform: translateX(-100%);
-    transition: all 1s;
+    
 }
-.anim2 {
+.anim-left, .anim-left2 {
     opacity: 0;
     filter: blur(5px);
     transform: translateX(100%);
-    transition: all 1s;
+    
 }
+
 
 
 
---
+++ src/components/timeline.svelte
@@ -0,0 +1,42 @@
+<section class="">
+	<div class="container max-w-5xl px-4 py-12 mx-auto">
+		<div class="grid gap-4 mx-4 sm:grid-cols-12">
+			<div class="col-span-12 sm:col-span-3">
+				<div class="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:bg-slate-200">
+					<h3 class="text-slate-200 text-3xl font-semibold">Education Timeline</h3>
+					<span class="text-sm font-bold tracki uppercase dark:text-gray-400"></span>
+				</div>
+			</div>
+			<div class="relative col-span-12 px-4 space-y-6 sm:col-span-9">
+				<div class="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:dark:bg-gray-700">
+					<div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-slate-200">
+						<h3 class="text-slate-200 text-xl font-semibold tracki">Graduated HighSchool</h3>
+						<time class="text-xs tracki uppercase dark:text-gray-400">Sep 2018 - Jul 2022</time>
+						<p class="text-slate-300 mt-3">In 2018 I started HighSchool on Havo level 2022 at the Passie in Lunneten, The netherlands. In 2022 I graduated on the VMBO-TL level. </p>
+					</div>
+					<div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-slate-200">
+						<h3 class="text-slate-200 text-xl font-semibold tracki"> Camebrigde English Course</h3>
+						<time class="text-xs tracki uppercase dark:text-gray-400">Mar 2022</time>
+						<p class="text-slate-300 mt-3"> In 2022 I got a 'First Certificate in English' on B2 niveau from Camebrigde univeristy.
+							In this course I had to do a lot of reading, writing, listening and speaking, which helped me a lot with my english skills.
+						</p>
+					</div>
+					<div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-slate-200">
+						<h3 class="text-slate-200 text-xl font-semibold tracki">Pellentesque habitant morbi</h3>
+						<time class="text-xs tracki uppercase dark:text-gray-400">Jan 2016</time>
+						<p class="text-slate-300 mt-3">Suspendisse tincidunt, arcu nec faucibus efficitur, justo velit consectetur nisl, sit amet condimentum lacus orci nec purus. Mauris quis quam suscipit, vehicula felis id, vehicula enim.</p>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<style>
+    h3{
+    font-family: league;
+  }
+</style>
+			
+
